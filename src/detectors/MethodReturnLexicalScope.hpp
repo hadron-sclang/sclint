a56b4e011f2d549419a076c30e7a23d21a007167
Luke Nihlen <luke.nihlen@gmail.com>
2022-10-10 09:58:13 -0400

    Add detector framework and Scoped Method Return detector (#1)

---
+++ src/detectors/MethodReturnLexicalScope.hpp
@@ -0,0 +1,106 @@
+#ifndef SCLINT_SRC_DETECTORS_METHOD_RETURN_LEXICAL_SCOPE_HPP_
+#define SCLINT_SRC_DETECTORS_METHOD_RETURN_LEXICAL_SCOPE_HPP_
+
+#include "Detector.hpp"
+
+#include "SCParserBaseListener.h"
+
+#include <string>
+
+namespace sclint {
+
+/*
+Test Code:
+
+True Positive:
+Function {
+	prTry {
+		var result, thread = thisThread;
+		var next = thread.exceptionHandler,
+		wasInProtectedFunc = Exception.inProtectedFunction;
+		thread.exceptionHandler = {|error|
+			thread.exceptionHandler = next; // pop
+			^error  // HERE
+		};
+		Exception.inProtectedFunction = true;
+		result = this.value;
+		Exception.inProtectedFunction = wasInProtectedFunc;
+		thread.exceptionHandler = next; // pop
+		^result
+	}
+}
+
+False Positive:
+Quark {
+	dependencies {
+		var deps = this.data['dependencies'] ?? {^[]};  // HERE
+		if(deps.isSequenceableCollection.not, {
+			("Invalid dependencies " + this + deps).warn;
+			^[]
+		});
+		^deps.collect({ |dep|
+			var q = Quark.parseDependency(dep, this);
+			if(q.isNil, {
+				"% not found".format(dep).warn;
+			});
+			q
+		}).select({ |it| it.notNil });
+	}
+}
+*/
+
+class MethodReturnLexicalScope : public Detector {
+public:
+    MethodReturnLexicalScope():
+        Detector(),
+        m_inBlockCount(0),
+        m_inNamedAssignCount(0),
+        m_inArgsCount(0) {}
+    virtual ~MethodReturnLexicalScope() = default;
+
+    void enterBlock(sprklr::SCParser::BlockContext*) override { ++m_inBlockCount; }
+    void exitBlock(sprklr::SCParser::BlockContext*) override { --m_inBlockCount; }
+
+    void enterBody(sprklr::SCParser::BodyContext* ctx) override {
+        if (!ctx->returnExpr() || m_inArgsCount > 0 || m_inBlockCount == 0 || m_inNamedAssignCount == 0)
+            return;
+
+        m_issues->emplace_back(ctx->returnExpr()->start->getLine(), ctx->returnExpr()->start->getCharPositionInLine(),
+            std::string("method return in named function"));
+    }
+
+    void enterExprAssignDotName(sprklr::SCParser::ExprAssignDotNameContext*) override { ++m_inNamedAssignCount; }
+    void exitExprAssignDotName(sprklr::SCParser::ExprAssignDotNameContext*) override { --m_inNamedAssignCount; }
+
+    void enterExprAssignGlobal(sprklr::SCParser::ExprAssignGlobalContext*) override { ++m_inNamedAssignCount; }
+    void exitExprAssignGlobal(sprklr::SCParser::ExprAssignGlobalContext*) override { --m_inNamedAssignCount; }
+
+    void enterExprAssign(sprklr::SCParser::ExprAssignContext*) override { ++m_inNamedAssignCount; }
+    void exitExprAssign(sprklr::SCParser::ExprAssignContext*) override { --m_inNamedAssignCount; }
+
+    void enterVarDef(sprklr::SCParser::VarDefContext*) override { ++m_inNamedAssignCount; }
+    void exitVarDef(sprklr::SCParser::VarDefContext*) override { --m_inNamedAssignCount; }
+
+    // Block arguments to messages are safe to contain method returns because the method return is guaranteed to
+    // respect the caller frame.
+    void enterBlockArgList(sprklr::SCParser::BlockArgListContext*) override { ++m_inArgsCount; }
+    void exitBlockArgList(sprklr::SCParser::BlockArgListContext*) override { --m_inArgsCount; }
+
+    void enterArgList(sprklr::SCParser::ArgListContext*) override { ++m_inArgsCount; }
+    void exitArgList(sprklr::SCParser::ArgListContext*) override { --m_inArgsCount; }
+
+    void enterKeyArgList(sprklr::SCParser::KeyArgListContext*) override { ++m_inArgsCount; }
+    void exitKeyArgList(sprklr::SCParser::KeyArgListContext*) override { --m_inArgsCount; }
+
+    void enterExprBinop(sprklr::SCParser::ExprBinopContext*) override { ++m_inArgsCount; }
+    void exitExprBinop(sprklr::SCParser::ExprBinopContext*) override { --m_inArgsCount; }
+
+private:
+    int m_inBlockCount;
+    int m_inNamedAssignCount;
+    int m_inArgsCount;
+};
+
+} // namespace sclint
+
+#endif // SCLINT_SRC_DETECTORS_METHOD_RETURN_LEXICAL_SCOPE_HPP_
---
+++ src/sclint.cpp
@@ -0,0 +1,74 @@
+#include "detectors/Detector.hpp"
+#include "detectors/MethodReturnLexicalScope.hpp"
+#include "detectors/Whitespace.hpp"
+
+#include "fmt/format.h"
+#include "gflags/gflags.h"
+#include "SCLexer.h"
+#include "SCParser.h"
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+
+namespace {
+
+std::unique_ptr<char[]> readFile(std::string fileName, size_t& codeSize) {
+    codeSize = std::filesystem::file_size(fileName);
+    auto code = std::make_unique<char[]>(codeSize);
+    std::ifstream inFile(fileName, std::ifstream::binary);
+    if (!inFile) {
+        std::cerr << "Error opening input file: " << fileName << "\n";
+        return nullptr;
+    }
+    inFile.read(code.get(), codeSize);
+    if (!inFile) {
+        std::cerr << "Error reading input file: " << fileName << "\n";
+        return nullptr;
+    }
+    inFile.close();
+    return code;
+}
+
+} // namespace
+
+int main(int argc, char* argv[]) {
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    if (argc != 2) {
+        std::cerr << "Missing input file path.\n";
+        return -1;
+    }
+
+    auto fileName = std::string(argv[1]);
+    size_t codeSize = 0;
+    auto code = readFile(fileName, codeSize);
+    if (!code)
+        return -1;
+
+    antlr4::ANTLRInputStream input(code.get(), codeSize);
+    sprklr::SCLexer lexer(&input);
+    antlr4::CommonTokenStream tokens(&lexer);
+    sprklr::SCParser parser(&tokens);
+    auto parseTree = parser.root();
+
+    if (parser.getNumberOfSyntaxErrors()) {
+        std::cerr << fmt::format("{} had {} syntax errors\n", fileName, parser.getNumberOfSyntaxErrors());
+        return -1;
+    }
+
+    sclint::DetectorMux mux;
+    mux.addDetector(std::make_unique<sclint::MethodReturnLexicalScope>());
+    mux.addDetector(std::make_unique<sclint::Whitespace>(&tokens));
+
+    antlr4::tree::ParseTreeWalker::DEFAULT.walk(&mux, parseTree);
+
+    for (const auto& issue : mux.issues()) {
+        std::cout << fmt::format("{} line {} col {}: {}\n", fileName, issue.lineNumber, issue.columnNumber,
+            issue.message);
+    }
+
+    return 0;
+}
